0cb67e282501156ddba0d9fcf9f50a65997e986d
vidmo <104494134+VidmoVDZ@users.noreply.github.com>
2024-09-04 20:31:07 +0200

    Update decode.dart

--- decode.dart
+++ decode.dart
@@ -1,29 +1,8 @@
 import 'init.dart';
 import 'code.dart';
 import 'dart:async';
-// String decode(String password, List pattern)
-// {
-// 	String decodedPassword = "";
 
-
-// 	for (String item in password.split("")) 
-// 	{
-// 		if (pattern.indexOf(item) - jump() < 0)
-// 		{
-// 			print(pattern.indexOf(item) + jump().abs());
-// 			decodedPassword += pattern[(pattern.indexOf(item) - jump()).abs()];
-// 		}
-// 		else
-// 		{
-// 			decodedPassword += pattern[pattern.indexOf(item) - jump()];
-// 		}
-// 	}
-
-
-// 	return decodedPassword;
-// }
-
-Future<String> decode(String password, List pattern)
+Future<String> decode(String password, List pattern) async // just reversing coded password!
 {
     String decodedPassword = "";
 
